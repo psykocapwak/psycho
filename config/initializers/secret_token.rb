05c065bbc115ac3e3a98983a498f908f368e52ee
geoffreyantoine <geoffrey.antoine@neoweb.fr>
2012-03-17 16:29:35 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psycho::Application.config.secret_token = '632e8d46e901ce34a75c12df293532a446900e104531693a5c8940d45a6d2823a8d36a487e430ada59dab3a381d818e03dc4ddd8dbac2ec9eb2127ff7157d862'
