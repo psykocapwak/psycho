dc624cde77a001bdb9bc0ce0aa9a3482165d8895
geoffreyantoine <geoffrey.antoine@neoweb.fr>
2012-03-18 20:54:29 +0100

    first step

--- Gemfile
+++ Gemfile
@@ -5,7 +5,10 @@ gem 'rails', '3.2.1'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+#gem 'sqlite3'
+gem "pg"
+gem "nifty-generators", :group => :development
+gem "thin"
 
 
 # Gems used only for assets and not required
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def title(page_title, show_title = true)
+    @show_title = show_title
+    @content_for_title = page_title.to_s
+  end
+
+  def show_title?
+    @show_title
+  end
+
+  def stylesheet(*args)
+    content_for(:head) { stylesheet_link_tag(*args) }
+  end
+
+  def javascript(*args)
+    content_for(:head) { javascript_include_tag(*args) }
+  end
 end
