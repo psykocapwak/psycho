889832497684ee87865a61c523709572c6e0022d
geoffreyantoine <geoffrey.antoine@neoweb.fr>
2012-03-19 21:35:54 +0100

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -3,6 +3,7 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'http://www.larrousse.net'
 SitemapGenerator::Sitemap.create do
+  add '/', :changefreq => 'daily', :priority => 0.9
   add '/psykocapwak/simple-cap', :changefreq => 'daily', :priority => 0.9
   add '/psykocapwak/swim-cap', :changefreq => 'daily', :priority => 0.9
   add '/psykocapwak/cat-cap', :changefreq => 'daily', :priority => 0.9
